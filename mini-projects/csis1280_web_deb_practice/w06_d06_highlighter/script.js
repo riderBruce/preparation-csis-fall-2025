6d266ddf8f9107d12093689003ef72c5a96511e5
Youngil Kim <skasekase@gmail.com>
2025-08-06 21:06:26 +0900

    multiple highlight color

--- mini-projects/csis1280_web_deb_practice/w06_d06_highlighter/script.js
+++ mini-projects/csis1280_web_deb_practice/w06_d06_highlighter/script.js
@@ -1,6 +1,20 @@
 const highlightBtn = document.querySelector(".btn-highlight");
+const redBtn = document.querySelector(".color-selector.r");
+const greenBtn = document.querySelector(".color-selector.g");
+const blueBtn = document.querySelector(".color-selector.b");
+let currentColor = "d";
+let highlightList = [];
 
 highlightBtn.addEventListener("click", highlightText);
+redBtn.addEventListener("click", () => {
+  changeColor("r");
+});
+greenBtn.addEventListener("click", () => {
+  changeColor("g");
+});
+blueBtn.addEventListener("click", () => {
+  changeColor("b");
+});
 
 function highlightText() {
   const selection = window.getSelection();
@@ -11,10 +25,38 @@ function highlightText() {
     if (selectedText.length > 0) {
       const mark = document.createElement("mark");
       mark.textContent = selectedText;
+      mark.classList.add(currentColor);
 
       range.deleteContents();
       range.insertNode(mark);
+      
       selection.removeAllRanges();
     }
   }
+}
+
+function changeColor(color) {
+  highlightBtn.classList.remove("d");
+  highlightBtn.classList.remove("r");
+  highlightBtn.classList.remove("g");
+  highlightBtn.classList.remove("b");
+  highlightBtn.classList.add(color);
+  currentColor = color;
+}
+
+function colorSelector(color) {
+  switch (color) {
+    case "red":
+      return "r";
+    case "green":
+      return "g";
+    case "blue":
+      return "b";
+    default:
+      return "Invalid color";
+  }
+}
+
+function selectCurrentColor() {
+  return window.getComputedStyle(highlightBtn).backgroundColor;
 }
